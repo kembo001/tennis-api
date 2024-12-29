fd88af4d2970966a69532fb19dd07623a7eddcc0
Brandon <brandonkemboi87@gmail.com>
2024-12-29 14:43:00 -0600

    New host

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
+    host: "44.230.95.183",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
